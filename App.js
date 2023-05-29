389dc5d7c8890ea0391327c48b8dc470fdeaa472
Valentine N <85905701+vallyshizzle@users.noreply.github.com>
2023-05-29 04:43:45 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import { Navbar, Hero, Projects, About, Testimonial, Blog, Footer} from './components'
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Projects />
+    <About />
+    <Testimonial />
+    <Blog />
+    <Footer />
+    </>
+    
+  );
+}
+
+export default App;
